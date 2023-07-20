bfed7546c084b2426c856a449ab60c26e7953d86
Mr. IIOT(nick) <Mr. IIOT@LAPTOP-LLL592JF>
2023-07-20 09:21:37 -0700

    forgot to run comand add

---
+++ fanuc/collectors/Macro.cs
@@ -0,0 +1,43 @@
+﻿using l99.driver.fanuc.strategies;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// ReSharper disable once CheckNamespace
+namespace l99.driver.fanuc.collectors;
+
+
+public class Macro : FanucMultiStrategyCollector
+{
+    public Macro(FanucMultiStrategy strategy, object configuration) : base(strategy, configuration)
+    {
+    }
+
+    public override async Task InitPathsAsync()
+    {
+        await Strategy.Apply(typeof(veneers.Macro), "macro");
+    }
+
+    public override async Task CollectForEachPathAsync(short currentPath, string[] axis, string[] spindle,
+        dynamic pathMarker)
+    {
+ 
+        foreach (var macroEntry in Configuration)
+        {
+            if (macroEntry.ContainsKey("id"))
+            {
+                var id = macroEntry["id"];
+                short num = (short)macroEntry["number"];
+
+                await Strategy.Peel("macro",
+                    new[]
+                    {
+                    await Strategy.SetNativeKeyed(id, await Strategy.Platform.RdMacroAsync(id, num, 10)),
+                    },
+                    new dynamic[]
+                    {
+                    });
+            }
+        }
+
+
+    }
+}
---
+++ fanuc/veneers/Macro.cs
@@ -0,0 +1,46 @@
+﻿using System.Dynamic;
+using l99.driver.@base;
+
+// ReSharper disable once CheckNamespace
+namespace l99.driver.fanuc.veneers;
+
+public class Macro : Veneer
+{
+    public Macro(Veneers veneers, string name = "", bool isCompound = false, bool isInternal = false) : base(
+        veneers, name, isCompound, isInternal)
+    {
+
+    }
+    protected override async Task<dynamic> AnyAsync(dynamic[] nativeInputs, dynamic[] additionalInputs)
+    {
+        if (nativeInputs.All(o => o.success == true))
+        {
+            var datano = nativeInputs[0].response.cnd_rdmacro.macro.datano;
+            var mcr_val =  nativeInputs[0].response.cnd_rdmacro.macro.mcr_val;
+            var id = nativeInputs[0].response.id;
+         //   var size = nativeInputs[0].response.size;
+
+            dynamic state = new ExpandoObject();
+           //state.id = id;
+            state.datano = datano;
+            state.mcr_val = mcr_val;
+
+            Dictionary<dynamic, dynamic> dict = new Dictionary<dynamic, dynamic>();
+            dict.Add(id, state);
+
+            dynamic currentValue = new ExpandoObject();
+            // convert state list to dictionary
+            currentValue.Macro = dict;
+
+            await OnDataArrivedAsync(nativeInputs, additionalInputs, currentValue);
+
+            if (((object)currentValue).IsDifferentString((object) LastChangedValue))
+                await OnDataChangedAsync(nativeInputs, additionalInputs, currentValue);
+        }
+        else
+        {
+            await OnHandleErrorAsync(nativeInputs, additionalInputs);
+        }
+        return new { veneer = this };
+    }
+}
